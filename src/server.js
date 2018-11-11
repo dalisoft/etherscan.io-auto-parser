c85225144f76176526b9e9c790bddaeaa80c2ce8
dalisoft <dalisoft@mail.ru>
2018-11-11 18:58:48 +0500

    try to fetch items from 100 items per page to reduce request to server

--- src/server.js
+++ src/server.js
@@ -5,7 +5,7 @@ dotEnv.config();
 const puppeteer = require('puppeteer');
 const { endpoints } = require('../src/config');
 const { receiptNormalizer: normalizeHTML } = require('../src/axios');
-const { sleep, dbHelper } = require('../src/helpers');
+const { dbHelper } = require('../src/helpers');
 const fs = require('fs');
 
 (async () => {
@@ -15,7 +15,20 @@ const fs = require('fs');
     const page = await browser.newPage();
     await page.goto(endpoints.scanUrl);
 
+    await page.evaluate(() => {
+        const selectElem = document.querySelector('select[name="ctl00$ContentPlaceHolder1$ddlRecordsPerPage"]');
+        const optionElem = selectElem.querySelector('option:nth-child(4)');
+        optionElem.selected = true;
+        const event = new Event('change', {bubbles: true});
+        selectElem.dispatchEvent(event);
+    });
+
+    await page.waitForSelector('select[name="ctl00$ContentPlaceHolder1$ddlRecordsPerPage"]');
+
     const { page: pages, data } = await page.evaluate(() => ({data: document.documentElement.innerHTML})).then(normalizeHTML);
+
+    // const pages = 1; // Uncomment when need specified amount of page that should be fetched
+    let currentPage = 0; // When you want fetch from specified page range
 
     const firstPage = fs.existsSync(`./db/pages/${1}.db`);
 
@@ -24,13 +37,16 @@ const fs = require('fs');
         dbHelper.insert(_db, data);
     }
 
+    if (pages < 1) {
+        return false;
+    }
 
     await Promise.all(new Array(pages - 1)
         .fill(endpoints.scanUrl)
-        .map((url, i) => ({ url: `${url}/${pages - i}`, page: pages - i }))
+        .map((url, i) => ({ url: `${url}/${pages - currentPage - i}`, page: pages - currentPage - i }))
         .filter(({ page }) => !fs.existsSync(`./db/pages/${page}.db`))
-        .map(async ({ url, page: pageId }, i) => {
-            await sleep(i * 1500);
+        .map(async ({ url, page: pageId }) => {
+            await page.waitForSelector('select[name="ctl00$ContentPlaceHolder1$ddlRecordsPerPage"]');
 
             await page.goto(url);
             const { data } = await page.evaluate(() => ({data: document.documentElement.innerHTML})).then(normalizeHTML).catch(() => null);
